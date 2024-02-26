d7bd1534af0c0fb8373f356e08d45e411678b312
BrunoWeezyy <neidinidnid@hotmail.com>
2024-02-26 00:24:13 -0300

    title

--- site-responsivo/site/site/js/script.js
+++ site-responsivo/site/site/js/script.js
@@ -13,7 +13,20 @@ darkmode.init();
 const portfolio = new Portfolio('.zoom-text', '.img-container', '.img_gallery','.image-view','next-btn', 'prev-btn', 'close', '.image-box')
 portfolio.init();
 
-initMenuMobile()
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    // Get form values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    // Do something with the form values (e.g., send them to a server)
+    console.log('Nome:', name);
+    console.log('Email:', email);
+
+    // You can add more functionality here, like sending the form data to a server using AJAX
+});
+
 initMenuLi()
 initTyping()
 initAnimationScroll()
